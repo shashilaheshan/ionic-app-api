9eab104b5a452b05384768dd60fe1ef78d21aa7d
shashila heshan <ebayshashila@gmail.com>
2018-10-01 10:51:38 +0530

    fixing api issue with headers
    fixing api issue with headers

--- src/app/services/sib.service.ts
+++ src/app/services/sib.service.ts
@@ -1,18 +1,22 @@
-import {Injectable} from '@angular/core';
-import {Http} from '@angular/http';
+import {Injectable, Type} from '@angular/core';
+import {Http,Headers} from '@angular/http';
 
 import 'rxjs/Rx';
+import { Content } from 'ionic-angular';
 
 @Injectable()
 export class SibService{
 public http:any;
 baseUrl: String;
+
   constructor( http:Http){
+   
 this.http=http;
 this.baseUrl="https://sibapp.000webhostapp.com/api/getPromotions";
 
   }
   getPromotions(){
+   
     
      return this.http.get(this.baseUrl)
      .map(res=> res.json());
